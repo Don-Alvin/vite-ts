6c2f315900d34e9b000087c9e8db205ab79d76a7
Don Alvin <alvindon41@gmail.com>
2024-02-20 11:46:16 +0300

    set up react project

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react"
-import Counter from "./components/Counter"
-import Heading from "./components/Heading"
-import Section from "./components/Section"
-import List from "./List"
-
 const App = () => {
-  const [count, setCount] = useState(0)
+
   return (
-    <>
-      <Heading title={'Hello'} />
-      <Section title={'Hello again'}>This is my section</Section>
-      <Counter count={count} setCount={setCount} />
-      <List items={['Coffee', "Tacos", "Code"]} render={(item: string) => <span className="gold">{item}</span>} />
-    </>
+    <div>
+      App
+    </div>
   )
 }
 
